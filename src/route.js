20fb3f3257d798da946693f468bc35df303acf4d
fatz <fatz.zhang@gmail.com>
2019-06-17 22:48:49 +0800

    modify route decide
    compare pattern parts with url parts, and put only params that match '@' part into callback

--- src/route.js
+++ src/route.js
@@ -1,10 +1,14 @@
+import helper from './helper';
+
 class Route {
   /**
    * @param  [string] pattern route pattern, e.g. @a/@b/@c
    * @param  [string] act     action after hit
    */
   constructor(pattern, act) {
-    this.pattern = pattern.replace('/', '');
+    const trimedPattern = helper.trim(pattern);
+
+    this.parts = trimedPattern.split('/');
 
     if (typeof act === 'function') {
       this.act = act;
@@ -13,24 +17,35 @@ class Route {
 
   /**
    * Traveler will call this when get a new redirect path
-   * @param  [url] url
+   * @param  [string] url trimed url
    * @return [bool]
    */
   decide(url) {
-    const parts = url.split('/');
-    const params = this.pattern.split('@');
+    const urlParts = url.split('/');
+    const params = [];
 
-    params.shift();
-
-    if (parts.length === params.length) {
-      if (typeof this.act === 'function') {
-        this.act(...parts); // 分別帶入各個參數
-      }
-
-      return true;
+    if (urlParts.length !== this.parts.length) {
+      return false;
     }
 
-    return false;
+    for (let i = urlParts.length; i < urlParts.length; i += 1) {
+      const isParam = /^@/.test(this.parts[i]);
+
+      if (!isParam && (urlParts[i] !== this.parts[i])) {
+        return false;
+      }
+
+      if (isParam) {
+        params.push(urlParts[i]);
+      }
+    }
+
+
+    if (typeof this.act === 'function') {
+      this.act(...params); // put the url parts which has '@' into the callback
+    }
+
+    return true;
   }
 }
 
